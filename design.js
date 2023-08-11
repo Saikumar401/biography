094a0d956d53e561747d29dbc1d1f1673c61b7c9
Saikumar401 <121429113+Saikumar401@users.noreply.github.com>
2023-08-11 15:00:35 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,24 @@
+ReactDOM.render(<div><h3>This is a Biography of Jordan Walke : </h3>
+<p>Jordan Walke, who is the creator of ReactJS recently announced in the micro-blogging platform, that after a decade he is leaving Facebook. Walke had been working as a software engineer at the social media giant, where he was working on the React JavaScript library.
+            In a post, Walke tweeted that after spending more than a decade at the social media giant, he is finally leaving the company. However, there lies a good reason for this resignation. All these years, after working for a company, he is now going to start one of his own.
+            However, the details of the company have not been disclosed yet. 
+            Jordan deserves all the credit for creating React. He was influenced by XHP, a PHP-based component system that is still in use at Facebook, but also by functional programming ideas.
+            Pete Hunt wanted to use React at Instagram, so he helped to extract React from Facebook-specific code. This prepared React to be open sourced.
+            Later, Facebook put a team of engineers behind React and also received great contributions from the open source community. Significant contributors include Sebastian Markbåge and Ben Alpert, among many others.
+            </p>
+            <ul>
+                <li>Invension:ReactJs</li>
+                <li>Employee of: Facebook company</li>
+            </ul>
+            <ol>
+                <li>inventer</li>
+                <li>reactjs owner</li>
+                <li>achiever</li>
+            </ol>
+            <dl>
+    <dt>saikumar</dt>
+    <dd>-Alias narayana</dd>
+</dl>
+<img src="https://149695847.v2.pressablecdn.com/wp-content/uploads/2021/01/pasted-image-0-2.png" width="700px"></img>
+<h4>This is a logo of 'ReactJs' invented by "Jordan Walke"</h4>
+</div>,document.getElementById("content"));
